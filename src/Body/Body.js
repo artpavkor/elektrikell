151d891486994cd381978c2b241b934d2169e4ea
Artem Korniienko <artpavkor@gmail.com>
2023-02-16 20:40:26 +0200

    API, useEffect

--- src/Body/Body.js
+++ src/Body/Body.js
@@ -1,41 +1,45 @@
+import { useState, useEffect } from "react";
 import { ResponsiveContainer, LineChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Line } from "recharts";
+import { getPriceData } from "../services/apiService";
+import ErrorModal from "../ErrorModal";
+import moment from "moment";
 
-function Body () {
-    const data = [
-        {
-            name: 'Page A',
-            pv: 2400,
-            uv: 4000,
-        },
-        {
-            name: 'Page B',
-            pv: 1400,
-            uv: 3000,
-        },
-        {
-            name: 'Page C',
-            pv: 9800,
-            uv: 2000,
-        },
-        {
-            name: 'Page D',
-            pv: 3900,
-            uv: 2800,
-        }
-    ]
+function Body() {
+    const [data, setData] = useState([]);
+    const [errorMessage, setErrorMessage] = useState(null);
+
+    useEffect(() => {
+        getPriceData()
+            .then(( { success, data, message }) => {
+                if (!success) {
+                    throw message[0];
+                }
+                const newData = data.ee.map(d => {
+                    return {
+                        price: d.price,
+                        hour: moment.unix(d.timestamp).hour()
+                    }
+                });
+                setData(newData);
+            })
+            .catch((error) => setErrorMessage(error.toString));
+    }, []);
+    
     return (
-        <ResponsiveContainer width='100%' height={400}>
-         <LineChart data={data}
-        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="pv" stroke="#8884d8" />
-        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-    </LineChart>
-    </ResponsiveContainer>
+        <>
+            <ResponsiveContainer width='100%' height={400}>
+                <LineChart data={data}
+                    margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="hour" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey="price" stroke="#8884d8" />
+                </LineChart>
+            </ResponsiveContainer>
+            <ErrorModal errorMessage={errorMessage} handleClose={() => setErrorMessage(null)}/>
+        </>
     )
 }
 
