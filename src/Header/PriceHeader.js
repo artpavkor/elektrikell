4c9d8a97bae35c23747c77988c327524eb24057b
Artem Korniienko <artpavkor@gmail.com>
2023-02-16 16:22:59 +0200

    Домашнее задание, elektri hind hetkel, logo

--- src/Header/PriceHeader.js
+++ src/Header/PriceHeader.js
@@ -5,12 +5,19 @@ import SelectPriceType from './SelectPriceType';
  
 function PriceHeader(props) {
   return (
-      <Row className='mb-2 text-center'>
-        <Col>1 of 3</Col>
-        <Col> 
-        <SelectPriceType {...props} />
+      <Row className='mb-5'>
+        <Col>
+        <span className='fs-5 ms-3'>Elektri hind hetkel on</span> <br/>
+        <span className='fs-5 ms-3  p-1 rounded-2 span_red'>ikka väga kõrge</span>
         </Col>
-        <Col>3 of 3</Col>
+        <Col className='text-center'> 
+        <br/>
+        <SelectPriceType {...props}/>
+        </Col>
+        <Col className='text-end'>
+        <h3 className='fs-1'>19.78</h3> 
+        <p>senti / kilovatt-tund</p>
+        </Col>
       </Row>
   );
 }
--- src/Header/SelectPriceType.js
+++ src/Header/SelectPriceType.js
@@ -4,8 +4,10 @@ import ButtonGroup from 'react-bootstrap/ButtonGroup';
 function SelectPriceType({activePrice, setActivePrice}) {
   return (
     <ButtonGroup aria-label="Basic example">
-      <Button active ={activePrice === 'low'} onClick = {() => setActivePrice('low')}  variant="secondary">Odavad Tiputunnid </Button>
-      <Button active ={activePrice === 'high'} onClick = {() => setActivePrice('high')} variant="secondary">Tiputunnid</Button>
+      <div className='btn_group'> 
+      <Button active ={activePrice === 'low'} onClick = {() => setActivePrice('low')}  variant="btn btn-light border border-0">Odavad Tiputunnid </Button>
+      <Button active ={activePrice === 'high'} onClick = {() => setActivePrice('high')} variant="btn btn-light border border-0">Tiputunnid</Button>
+      </div>
     </ButtonGroup>
   );
 }
